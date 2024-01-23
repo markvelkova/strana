f75e34667ebaba0e5875613cf6c2aea099a8134e
markvelkova <157302159+markvelkova@users.noreply.github.com>
2024-01-23 17:32:20 +0100

    Create navigace.js

---
+++ navigace.js
@@ -0,0 +1,8 @@
+        <nav>
+        <ul id="rozcestnik">
+          <li class="cesta"><a href="index.html">AKTUALITY</a></li>
+          <li class="cesta"><a href="hidden.html">hidden</a></li>
+          <li class="cesta"><a href="treti.html">FOTO</a></li>
+          <li class="cesta_aktualni"><a href="javascr.html">VSTUP PRO ČLENY</a></li>
+          </ul>
+        </nav>
