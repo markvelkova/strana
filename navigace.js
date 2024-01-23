e59d46ec2e93485d2d130968f61a3003057daa22
markvelkova <157302159+markvelkova@users.noreply.github.com>
2024-01-23 17:54:34 +0100

    jenom consolelog

--- navigace.js
+++ navigace.js
@@ -1,11 +1,1 @@
-alert("Hello! I am an alert box!!");
-document.write('necotoudelalo');
-document.write('
-<nav>
-        <ul id="rozcestnik">
-          <li class="cesta"><a href="index.html">AKTUALITY</a></li>
-          <li class="cesta"><a href="hidden.html">hidden</a></li>
-          <li class="cesta"><a href="treti.html">FOTO</a></li>
-          <li class="cesta_aktualni"><a href="javascr.html">VSTUP PRO ČLENY</a></li>
-          </ul>
-        </nav>');
+console.log('Script loaded successfully!');
