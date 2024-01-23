a183d1eb3e63bffe70284190bc1db03eb3288a3e
markvelkova <157302159+markvelkova@users.noreply.github.com>
2024-01-23 17:37:52 +0100

    oprava syntaxu

--- navigace.js
+++ navigace.js
@@ -1,8 +1,10 @@
-        <nav>
+document.write('necotoudelalo');
+document.write('
+<nav>
         <ul id="rozcestnik">
           <li class="cesta"><a href="index.html">AKTUALITY</a></li>
           <li class="cesta"><a href="hidden.html">hidden</a></li>
           <li class="cesta"><a href="treti.html">FOTO</a></li>
           <li class="cesta_aktualni"><a href="javascr.html">VSTUP PRO ČLENY</a></li>
           </ul>
-        </nav>
+        </nav>');
